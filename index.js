22ccd9e1821556802713f4a46e63223b63907ae9
Gage Harmon <gaharmon1@gmail.com>
2023-01-31 09:48:39 -0700

    Functions continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(roller = 'roller-skate'){
+    return `This Saturday, I want to ${roller}!`;
+}
+
+
+let mondayWork = function(remote = "go to the office"){
+    return `This Monday, I will ${remote}.`;
+}
+
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
